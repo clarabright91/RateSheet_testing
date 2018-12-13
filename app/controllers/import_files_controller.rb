097015818ff7f7472f833750e64ec9cc975fb620
Vinod <vinodyuvasoft145@gmail.com>
2018-12-13 18:38:49 +0530

    add program for home ready sheet

--- app/controllers/import_files_controller.rb
+++ app/controllers/import_files_controller.rb
@@ -1,11 +1,11 @@
 class ImportFilesController < ApplicationController
-  before_action :get_bank, only: [:import_government_sheet, :programs]
+  before_action :get_bank, only: [:import_government_sheet, :programs, :import_homereddy_sheet]
 
   require 'roo'
   require 'roo-xls'
 
   def index
-    xlsx = Roo::Spreadsheet.open("/home/richa/richa/Kevin-Project/Pure-Loan/OB_New_Penn_Financial_Wholesale5806.xls")
+    xlsx = Roo::Spreadsheet.open("/home/yuva/Desktop/ratesheet/RateSheetExtractor/OB_New_Penn_Financial_Wholesale5806 (1).xls")
     begin
       xlsx.sheets.each do |sheet|
         if (sheet == "Cover Zone 1")
@@ -40,9 +40,8 @@ class ImportFilesController < ApplicationController
     end
   end
 
-
   def import_government_sheet
-    xlsx = Roo::Spreadsheet.open("/home/richa/richa/Kevin-Project/Pure-Loan/OB_New_Penn_Financial_Wholesale5806.xls")
+    xlsx = Roo::Spreadsheet.open("/home/yuva/Desktop/ratesheet/RateSheetExtractor/OB_New_Penn_Financial_Wholesale5806 (1).xls")
     xlsx.sheets.each do |sheet|
       if (sheet == "Government")
         sheet_data = xlsx.sheet(sheet)
@@ -94,6 +93,72 @@ class ImportFilesController < ApplicationController
     end
     redirect_to programs_import_file_path(@bank)
   end
+
+
+  def import_homereddy_sheet
+    xlsx = Roo::Spreadsheet.open("/home/yuva/Desktop/ratesheet/RateSheetExtractor/OB_New_Penn_Financial_Wholesale5806 (1).xls")
+    xlsx.sheets.each do |sheet|
+      if (sheet == "HomeReady")
+        sheet_data = xlsx.sheet(sheet)
+
+       (1..76).each do |r|
+            row = sheet_data.row(r)
+            if ((row.compact.count > 1) && (row.compact.count <= 3)) && (!row.compact.include?("California Wholesale Rate Sheet"))
+              rr = r + 1 # (r == 8) / (r == 36) / (r == 56)
+
+              max_column_section = row.compact.count - 1
+              (0..max_column_section).each do |max_column|
+
+                cc = 3 + max_column*6 # (3 / 9 / 15) 3/8/13
+
+                @title = sheet_data.cell(r,cc)
+                program_heading = @title.split
+
+
+                @term = program_heading[4] == "ARM" ? 0 : program_heading[3]
+                @interest_type = program_heading[5] == "Fixed" ? 0 : 2
+                if program_heading[3] == "5/1"
+                  @interest_subtype = 5
+                  elsif program_heading[3] == "7/1"
+                    @interest_subtype = 7
+                elsif program_heading[3] == "10/1"
+                  @interest_subtype = 10
+                end
+                @program = @bank.programs.find_or_create_by(title: @title)
+                @program.update(term: @term,interest_type: @interest_type, interest_subtype: @interest_subtype, loan_type: 0)
+                @block_hash = {}
+                key = ''
+                (0..50).each do |max_row|
+                  @data = []
+                  (0..4).each_with_index do |index, c_i|
+                    rrr = rr + max_row
+                    ccc = cc + c_i
+                    value = sheet_data.cell(rrr,ccc)
+                    if (c_i == 0)
+                      key = value
+                      @block_hash[key] = {}
+                    else
+                      # first_row[c_i]
+                      @block_hash[key][15*c_i] = value
+                    end
+                    @data << value
+                  end
+
+                  if @data.compact.length == 0
+                    break # terminate the loop
+                  end
+              end
+              @program.update(interest_points: @block_hash)
+
+            end
+          end
+        end
+      end
+    end
+    redirect_to programs_import_file_path(@bank)
+  end
+
+
 
   def programs
     @programs = @bank.programs
