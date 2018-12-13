dccce03208274a9310900ef98fedc978509bf332
Ankit Chaturvedi <ankityuvasoft206@gmail.com>
2018-12-13 13:33:50 +0530

    Upload sheet data

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
     member do
       get :programs
       get :import_government_sheet
+      get :import_freddie_fixed_rate
+      get :import_conforming_fixed_rate
+      get :home_possible
+      get :conforming_arms
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
