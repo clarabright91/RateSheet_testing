95c51a6a9a1f25619ab3d2a0bc87befe6b2971e1
Ritesh Banskar <riteshyuvasoft163@gmail.com>
2018-12-14 17:04:08 +0530

    Feature# added program for jumbo series f

--- app/controllers/import_files_controller.rb
+++ app/controllers/import_files_controller.rb
@@ -1,6 +1,6 @@
 class ImportFilesController < ApplicationController
 
-  before_action :get_bank, only: [:import_government_sheet, :programs, :import_freddie_fixed_rate, :import_conforming_fixed_rate, :home_possible, :conforming_arms, :lp_open_acces_arms, :lp_open_access_105, :lp_open_access, :du_refi_plus_arms, :du_refi_plus_fixed_rate_105, :du_refi_plus_fixed_rate, :dream_big, :high_balance_extra, :freddie_arms, :import_jumbo_sheet, :jumbo_series_d]
+  before_action :get_bank, only: [:import_government_sheet, :programs, :import_freddie_fixed_rate, :import_conforming_fixed_rate, :home_possible, :conforming_arms, :lp_open_acces_arms, :lp_open_access_105, :lp_open_access, :du_refi_plus_arms, :du_refi_plus_fixed_rate_105, :du_refi_plus_fixed_rate, :dream_big, :high_balance_extra, :freddie_arms, :import_jumbo_sheet, :jumbo_series_d,:jumbo_series_f]
 
   require 'roo'
   require 'roo-xls'
@@ -41,7 +41,6 @@ class ImportFilesController < ApplicationController
       # the required headers are not all present
     end
   end
-
 
   def import_government_sheet
     file = File.join(Rails.root,  'OB_New_Penn_Financial_Wholesale5806.xls')
@@ -854,6 +853,71 @@ class ImportFilesController < ApplicationController
                     else
                       @block_hash[key][15*c_i] = value
                     end
+                  end
+                  @data << value
+                end
+
+                if @data.compact.length == 0
+                  break # terminate the loop
+                end
+              end
+              @program.update(interest_points: @block_hash)
+            end
+          end
+        end
+      end
+    end
+    redirect_to programs_import_file_path(@bank)
+  end
+
+  def jumbo_series_f
+    file = File.join(Rails.root,  'OB_New_Penn_Financial_Wholesale5806.xls')
+    xlsx = Roo::Spreadsheet.open(file)
+    xlsx.sheets.each do |sheet|
+      if (sheet == "Jumbo Series_F")
+        sheet_data = xlsx.sheet(sheet)
+        (2..36).each do |r|
+          row = sheet_data.row(r)
+          if ((row.compact.count > 1) && (row.compact.count <= 3)) && (!row.compact.include?("California Wholesale Rate Sheet"))
+            # r == 7 / 35 / 55
+            rr = r + 1 # (r == 8)/ (r == 23)
+            max_column_section = row.compact.count - 1
+            (0..max_column_section).each do |max_column|
+              cc = 6 + max_column*6 # (6 / 12 / 18)
+              @title = sheet_data.cell(r,cc)
+              program_heading = @title.split
+              # @term =  program_heading[3]
+              if program_heading[5] == "ARM"
+                @interest_type = 2
+                if @title.scan(/\d+/)[0] == "5"
+                  @interest_subtype = @title.scan(/\d+/)[0]
+                elsif @title.scan(/\d+/)[0] == "7"
+                  @interest_subtype = @title.scan(/\d+/)[0]
+                elsif @title.scan(/\d+/)[0] == "10"
+                  @interest_subtype = @title.scan(/\d+/)[0]
+                end
+              elsif program_heading[5] == "Fixed"
+                @interest_type = 0
+              end
+              @program = @bank.programs.find_or_create_by(title: @title)
+              if @interest_subtype.present?
+                @program.update(term: @term,interest_type: @interest_type,loan_type: 0,interest_subtype: @interest_subtype )
+              else
+                @program.update(term: @term,interest_type: @interest_type,loan_type: 0)
+              end
+              @block_hash = {}
+              key = ''
+              (0..50).each do |max_row|
+                @data = []
+                (0..4).each_with_index do |index, c_i|
+                  rrr = rr + max_row
+                  ccc = cc + c_i
+                  value = sheet_data.cell(rrr,ccc)
+                  if (c_i == 0)
+                    key = value
+                    @block_hash[key] = {}
+                  else
+                    @block_hash[key][15*c_i] = value
                   end
                   @data << value
                 end
