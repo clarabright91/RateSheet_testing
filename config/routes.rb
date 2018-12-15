5478fb0bf92bf65259ceeb119ac46802c38b9d34
Ankit Chaturvedi <ankityuvasoft206@gmail.com>
2018-12-15 16:03:16 +0530

    Upload Sheet data

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,13 @@ Rails.application.routes.draw do
       get :conforming_arms
       get :lp_open_acces_arms
       get :lp_open_access_105
+      get :lp_open_access
+      get :du_refi_plus_arms
+      get :du_refi_plus_fixed_rate_105
+      get :du_refi_plus_fixed_rate
+      get :dream_big
+      get :high_balance_extra
+      get :freddie_arms
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
