d88d67745a6dfe4c9bd6d21d922364cd9743ac2b
Neeraj Pathak <neerajyuvasoft196@gmail.com>
2019-01-08 19:53:00 +0530

    find base_rate in searching

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,32 +1,40 @@
 class DashboardController < ApplicationController
-  before_action :set_default, :find_programs
+  before_action :set_default, :find_base_rate
 
   def index
     @banks = Bank.all
     if request.post?
       set_variable
-      find_programs
+      find_base_rate
     end
   end
 
   def set_default
-    @loan_type = 1
+    @rate_type = "Fixed"
     @interest = "4.375"
-    @term = "10"
+    @term = 30
     @day = "30"
+    @base_rate = 0.0
   end
 
   def set_variable
-    @loan_type = params[:loan_type].to_i
+    @rate_type = params[:rate_type]
     @interest = params[:interest].to_s
-    @term = params[:term].to_s
-    @day = params[:day].to_s
+    @term = params[:term].to_i
+    @day = params[:day]
   end
 
-  def find_programs
-    programs =  Program.where(term: @term,loan_type: @loan_type)
-    @program = programs.find_by_title("VA 30 Yr Fixed")
-    @program.base_rate[@interest][@day]
+  def find_base_rate
+    programs = Program.where(term: @term, rate_type: @rate_type)
+    program = programs.find_by_program_name("Fannie Mae 30yr Fixed")
+    if program.present?
+      @adjustment =  program.adjustments.first
+      @adjustment_data = JSON.parse @adjustment.data
+      if program.base_rate[@interest].present?
+        @base_rate = program.base_rate[@interest][@day]
+      end
+    end
+    return @base_rate
   end
 
   render :index
