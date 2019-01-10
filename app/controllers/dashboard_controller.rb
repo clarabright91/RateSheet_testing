185b9c651239b24449fb3910e657de35648a1a4e
virendra rathore <virendrayuvasoft221@gmail.com>
2019-01-10 13:10:04 +0530

    add Loan Limit Type

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -18,6 +18,15 @@ class DashboardController < ApplicationController
     @program_name = "Fannie Mae 30yr Fixed"
     @sheet = "Cover Zone 1"
     @gov_sheet = "FHA"
+    @loan_limit_type = "High-Balance"
+    @fha = false
+    @va = false
+    @usda = false
+
+    @non_conforming = false
+    @conforming = false
+    @Jumbo = false
+    @high_balance = false
   end
 
   def set_variable
@@ -25,33 +34,83 @@ class DashboardController < ApplicationController
     @interest = params[:interest] if params[:interest].present?
     @lock_period = params[:lock_period] if params[:lock_period].present?
     @sheet = params[:sheet] if params[:sheet].present?
-    @gov_sheet = params[:gov] if params[:gov].present?
     if params[:rate_type] =="ARM" && params[:rate_type].present?
       @term = params[:term_arm].to_i if params[:term].present?
     else
       @term = params[:term].to_i if params[:term].present?
     end
+    
+    @gov_sheet = params[:gov] if params[:gov].present?
+    if @gov_sheet.present?
+      if @gov_sheet == "FHA"
+        @fha = true
+        @va = false
+        @usda = false
+      elsif @gov_sheet == "VA"
+        @va = true
+        @fha = false
+        @usda = false
+      elsif @gov_sheet == "USDA"
+        @usda = true
+        @va = false
+        @fha = false
+      else
+        @usda = false
+        @va = false
+        @fha = false
+      end
+    end
+
+    @loan_limit_type = params[:loan_limit_type] if params[:loan_limit_type].present?
+    if @loan_limit_type.present?
+      if @loan_limit_type == "Non-Conforming"
+        @non_conforming = true
+        @conforming = false
+        @Jumbo = false
+        @high_balance = false
+      elsif @loan_limit_type == "Conforming"
+        @non_conforming = false
+        @conforming = true
+        @Jumbo = false
+        @high_balance = false
+      elsif @loan_limit_type == "Jumbo"
+        @non_conforming = false
+        @conforming = false
+        @Jumbo = true
+        @high_balance = false
+      elsif @loan_limit_type == "High-Balance"
+        @non_conforming = false
+        @conforming = false
+        @Jumbo = false
+        @high_balance = true
+      else
+        @non_conforming = false
+        @conforming = false
+        @Jumbo = false
+        @high_balance = false
+      end
+    end
   end
 
   def find_base_rate
-    debugger
-    programs_list = Program.where(term: @term, rate_type: @rate_type)
-    program = programs_list.find_by_sheet_name(@sheet)
-    if program.present?
-      # Adjustment::MAIN_KEYS.key("FinancingType/LTV/CLTV/FICO")
-      @adjustment =  program.adjustments.first
-      @adjustment_data = JSON.parse @adjustment.data
-      @adjustment_data.keys.each do |adjustment_key|
-        if adjustment_key =="Conforming/RateType/Term/LTV/FICO"
-            @adjustment_data[adjustment_key]
-        end
-        @adjustment_data[adjustment_key]
-      end
+    
+    programs_list = Program.where(sheet_name: @sheet)
+     programs = programs_list.where(term: 30, rate_type: "Fixed", va:@va, fha: @fha, usda: @usda)
+    # if program.present?
+    #   # Adjustment::MAIN_KEYS.key("FinancingType/LTV/CLTV/FICO")
+    #   @adjustment =  program.adjustments.first
+    #   @adjustment_data = JSON.parse @adjustment.data
+    #   @adjustment_data.keys.each do |adjustment_key|
+    #     if adjustment_key =="Conforming/RateType/Term/LTV/FICO"
+    #         @adjustment_data[adjustment_key]
+    #     end
+    #     @adjustment_data[adjustment_key]
+    #   end
 
-      if program.base_rate[@interest].present?
-        @base_rate = program.base_rate[@interest][@lock_period]
-      end
-    end
+    #   if program.base_rate[@interest].present?
+    #     @base_rate = program.base_rate[@interest][@lock_period]
+    #   end
+    # end
     return @base_rate
   end
 
