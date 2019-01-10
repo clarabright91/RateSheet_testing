ba87e09494724202a836f18349c46489de610bde
Ankit Chaturvedi <ankityuvasoft206@gmail.com>
2019-01-10 17:23:20 +0530

    Fixed Freddie_fixed_rate issue

--- app/controllers/import_files_controller.rb
+++ app/controllers/import_files_controller.rb
@@ -311,6 +311,7 @@ class ImportFilesController < ApplicationController
       if (sheet == "Freddie Fixed Rate")
         sheet_data = xlsx.sheet(sheet)
         @sheet = sheet
+        main_key = ''
         (1..118).each do |r|
           row = sheet_data.row(r)
 
@@ -325,54 +326,59 @@ class ImportFilesController < ApplicationController
               @title = sheet_data.cell(r,cc)
 
               #term
-              @term = nil
+              term = nil
               program_heading = @title.split
               if @title.include?("10yr") || @title.include?("10 Yr")
-                @term = @title.scan(/\d+/)[0]
+                term = @title.scan(/\d+/)[0]
               elsif @title.include?("15yr") || @title.include?("15 Yr")
-                @term = @title.scan(/\d+/)[0]
+                term = @title.scan(/\d+/)[0]
               elsif @title.include?("20yr") || @title.include?("20 Yr")
-                @term = @title.scan(/\d+/)[0]
+                term = @title.scan(/\d+/)[0]
               elsif @title.include?("25yr") || @title.include?("25 Yr")
-                @term = @title.scan(/\d+/)[0]
+                term = @title.scan(/\d+/)[0]
               elsif @title.include?("30yr") || @title.include?("30 Yr")
-                @term = @title.scan(/\d+/)[0]
+                term = @title.scan(/\d+/)[0]
               end
 
               # interest type
               if @title.include?("Fixed")
-                @rate_type = "Fixed"
+                rate_type = "Fixed"
               elsif @title.include?("ARM")
-                @rate_type = "ARM"
+                rate_type = "ARM"
               elsif @title.include?("Floating")
-                @rate_type = "Floating"
+                rate_type = "Floating"
               elsif @title.include?("Variable")
-                @rate_type = "Variable"
+                rate_type = "Variable"
               else
-                @rate_type = nil
+                rate_type = nil
               end
 
               # conforming
+              conforming = false
               if @title.include?("Freddie Mac") || @title.include?("Fannie Mae") || @title.include?("Freddie Mac Home Possible") || @title.include?("Freddie Mac Home Ready")
-                @conforming = true
+                conforming = true
               end
 
               # freddie_mac
+              freddie_mac = false
               if @title.include?("Freddie Mac")
-                @freddie_mac = true
+                freddie_mac = true
               end
 
               # fannie_mae
+              fannie_mae = false
               if @title.include?("Fannie Mae") || @title.include?("Freddie Mac Home Ready")
-                @fannie_mae = true
+                fannie_mae = true
               end
 
               @program = @bank.programs.find_or_create_by(program_name: @title)
               @program_ids << @program.id
-              @program.update(term: @term,rate_type: @rate_type,loan_type: 0,conforming: @conforming,freddie_mac: @freddie_mac, sheet_name: @sheet)
+              @program.update(term: term,rate_type: rate_type,loan_type: "Purchase",conforming: conforming,freddie_mac: freddie_mac, sheet_name: sheet, fannie_mae: fannie_mae)
               @program.adjustments.destroy_all
               @block_hash = {}
               key = ''
+              main_key = term.to_s + "/"+ rate_type.to_s + "/InterestRate/LockPeriod"
+              @block_hash[main_key] = {}
               (0..50).each do |max_row|
                 @data = []
                 (0..4).each_with_index do |index, c_i|
@@ -381,10 +387,14 @@ class ImportFilesController < ApplicationController
                   value = sheet_data.cell(rrr,ccc)
                   if (c_i == 0)
                     key = value
-                    @block_hash[key] = {}
+                    @block_hash[main_key][key] = {}
                   else
+                    if @program.lock_period.length <= 3
+                      @program.lock_period << 15*c_i
+                      @program.save
+                    end
                     # first_row[c_i]
-                    @block_hash[key][15*c_i] = value
+                    @block_hash[main_key][key][15*c_i] = value
                   end
                   @data << value
                 end
@@ -393,7 +403,9 @@ class ImportFilesController < ApplicationController
                   break # terminate the loop
                 end
               end
-              @block_hash.shift
+              if @block_hash.values.first.keys.first.nil?
+                @block_hash.values.first.shift
+              end
               @program.update(base_rate: @block_hash)
             end
           end
