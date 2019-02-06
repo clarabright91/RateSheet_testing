4eeda951e2c8d1e60e636e60ce23017b4f59b274
Ankit Chaturvedi <ankityuvasoft206@gmail.com>
2019-02-06 19:08:37 +0530

    update new key

---
+++ db/migrate/20190205080327_add_fannie_mae_product_to_programs.rb
@@ -0,0 +1,6 @@
+class AddFannieMaeProductToPrograms < ActiveRecord::Migration[5.2]
+  def change
+    add_column :programs, :fannie_mae_product, :string
+    add_column :programs, :freddie_mac_product, :string
+  end
+end
