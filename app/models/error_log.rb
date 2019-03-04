71699fb1decd1fd429d80c92aec59e78962a69b1
Neeraj Pathak <neerajyuvasoft196@gmail.com>
2019-03-04 17:02:44 +0530

    Feature#Added validation for repeated data and modified error log display page

--- app/models/error_log.rb
+++ app/models/error_log.rb
@@ -1,7 +1,14 @@
 class ErrorLog < ApplicationRecord
   before_create :set_bank_name
-
+  validate :validate_repeated_data
   def set_bank_name
     self.bank_name = Bank.joins(:sheets).where("sheets.name Like ?", self.sheet_name).first.name
+  end
+
+  def validate_repeated_data
+    error_logs = self.class.where("sheet_name Like ? AND error_detail Like ? AND created_at > ?", self.sheet_name, self.error_detail, 24.hours.ago)
+    if error_logs.any?
+      errors.add(:created_at, 'Record is already exists')
+    end
   end
 end
