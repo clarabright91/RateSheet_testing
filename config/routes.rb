b1a9e9283898e0fbc00c3062934eb3e2fdb1a7f6
Neeraj Pathak <neerajyuvasoft196@gmail.com>
2019-03-05 12:35:57 +0530

    Feature# changed routes file according to new controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   # get 'error_logs/index'
   # get 'ob_american_financial_resources_wholesale5513/index'
   root :to => "dashboard#index"
-  # root :to => "import_files#index"
+  # root :to => "ob_new_rez_wholesale5806#index"
   get 'error_logs/*name', to: 'error_logs#display_logs', as: :display_logs
-  resources :import_files, :only => [:index] do
+  resources :ob_new_rez_wholesale5806, :only => [:index] do
     member do
       get :programs
       get :cover_zone_1
