98855b99f8d33150ac214181417d1e05c8ac1138
Neeraj Pathak <neerajyuvasoft196@gmail.com>
2019-03-05 16:27:20 +0530

    Feature#added error log link for each index page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def display_error_link sheet
+    return "<td class='error-button'><a class='btn btn-info btn-xs' href='/error_logs/#{sheet.name}'><span class='glyphicon glyphicon-edit'></span> Sheet Errors</a></td>"
+  end
 end
